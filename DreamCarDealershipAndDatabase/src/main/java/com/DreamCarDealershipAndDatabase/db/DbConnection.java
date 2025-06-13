e48ad918c55be053895cd3174eba22248ae325bd
Jesse Hernandez <101133566+jdez23@users.noreply.github.com>
2025-06-13 14:05:08 -0700

    fixed db creds

--- DreamCarDealershipAndDatabase/src/main/java/com/DreamCarDealershipAndDatabase/db/DbConnection.java
+++ DreamCarDealershipAndDatabase/src/main/java/com/DreamCarDealershipAndDatabase/db/DbConnection.java
@@ -9,8 +9,8 @@ public class DbConnection {
     private static final String URL =
             "jdbc:sqlserver://skills4it.database.windows.net:1433;" +  // Server address and port
                     "database=Courses;" +                                    // Target database name
-                    "user=gtareader@skills4it;" +                            // Username for authentication
-                    "password=StrongPass!2025;" +                           // Password (keep this secure!)
+                    "user=user418;" +                            // Username for authentication
+                    "password=YearupSecure2025!;" +                           // Password (keep this secure!)
                     "encrypt=true;" +                                        // Encrypt connection
                     "trustServerCertificate=false;" +                        // Do not trust self-signed certs
                     "loginTimeout=30;";
